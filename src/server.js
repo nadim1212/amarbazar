83779faebfa62982a3386d5b9f15daa3446dd877
nadim1212 <nadimniloy.swe@gmail.com>
2023-11-28 14:42:45 +0600

    first commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://monolog.nogodshop.com/api/v2";
-export const backemd_url = "https://monolog.nogodshop.com";
+export const server = "https://api.nogodshop.com/api/v2";
+export const backemd_url = "https://api.nogodshop.com";
 
 // export const server = "http://localhost:8000/api/v2";
 // export const backemd_url = "http://localhost:8000/";
